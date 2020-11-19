82b0a5c263598ba3b3ed1523c8457e7f89b18875
jeniferirwin <jenifer.irwin@gmail.com>
2020-11-19 00:26:26 -0500

    About to try installing a pipeline

---
+++ Assets/Scripts/Handlers/RoomController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using GreenBean.C64;
+
+namespace GreenBean.Handlers
+{
+    public class RoomController : MonoBehaviour
+    {
+        public Palette palette;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
