208225ddda3e8b1f6afae033daaca5e469295b26
jeniferirwin <jenifer.irwin@gmail.com>
2020-12-04 06:35:13 -0500

    Key now recognizes when player touches it

---
+++ Assets/Scripts/Core/Interfaces/ICollectible.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Com.Technitaur.GreenBean.Core
+{
+    public enum ItemType
+    {
+        RedKey,
+        CyanKey,
+        VioletKey,
+        Wand,
+        Sword,
+        Coin,
+        Torch    
+    }
+
+    public interface ICollectible
+    {
+        ItemType ItemType { get; }
+    }
+}
---
+++ Assets/Scripts/Inventory/Collectible.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Com.Technitaur.GreenBean.Core;
+
+namespace Com.Technitaur.GreenBean.Inventory
+{
+    public class Collectible : MonoBehaviour, ICollectible
+    {
+        public ItemType ItemType { get; }
+        [SerializeField] private ItemType itemType;
+
+        public void OnTriggerStay2D(Collider2D collider)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("Player has touched key");
+            }
+        }
+    }
+}
