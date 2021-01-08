e6b21954563af2ccbf01ef3f845bfbbe6b071c68
jeniferirwin <jenifer.irwin@gmail.com>
2021-01-07 22:07:02 -0500

    Tiles now change color at game start!

---
+++ Assets/Scripts/Tilemaps/InitializeLeveledTiles.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Com.Technitaur
+{
+    public class InitializeLeveledTiles : MonoBehaviour
+    {
+        private void Start()
+        {
+            var map = GetComponent<Tilemap>();
+            map.RefreshAllTiles();
+        }
+    }
+}
