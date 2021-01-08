75d7d62ab7194e670acba9bf4ba671082d662995
jeniferirwin <jenifer.irwin@gmail.com>
2021-01-07 20:09:24 -0500

    Slight cleanup of RoomData

--- Assets/Scripts/Core/RoomData.cs
+++ Assets/Scripts/Core/RoomData.cs
@@ -5,18 +5,15 @@ namespace Com.Technitaur.GreenBean.Core
     public class RoomData : MonoBehaviour, IRoom
     {
         public int level;
+
         public int Level
         {
             get
             {
                 if (level > 0 && level <= 10)
-                {
                     return level;
-                }
                 else
-                {
                     return 1;
-                }
             }
         }
     }
