e1b4b91990a6ed67bea30a09d46e3f2973d7292c
jeniferirwin <jenifer.irwin@gmail.com>
2021-01-23 22:38:33 -0500

    GREEN-84 Mob pathing is in! Doors begin with their own waypoints. The door's SetDirty disables those waypoints. Other waypoints must be placed manually. Waypoints are common to all mobs. Only spiders can use up/down waypoints.

---
+++ Assets/Scripts/Interactables/Mobs/Waypoint.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+namespace Com.Technitaur.GreenBean.Interactables
+{
+    public class Waypoint : MonoBehaviour
+    {
+        public bool isAbsolute;
+        public bool up;
+        public bool down;
+        public bool left;
+        public bool right;
+    }
+}
