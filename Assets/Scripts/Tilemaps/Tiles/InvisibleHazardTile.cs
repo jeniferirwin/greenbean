7eaa52f1fb029a8b9ae02477624d8b301d61c13c
jeniferirwin <jenifer.irwin@gmail.com>
2021-01-24 20:01:32 -0500

    Added new invisible hazard tile

---
+++ Assets/Scripts/Tilemaps/Tiles/InvisibleHazardTile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using System;
+using System.Collections.Generic;
+
+namespace Com.Technitaur.GreenBean.Tilemaps
+{
+    [CreateAssetMenu(fileName = "InvisibleHazardTile", menuName = "Invisible Hazard Tile", order = 1)]
+    public class InvisibleHazardTile : CustomTile, ICustomTile
+    {
+        public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+        {
+            tileData.sprite = null;
+        }
+    }
+}
