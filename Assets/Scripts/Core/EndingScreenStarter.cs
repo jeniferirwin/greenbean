13e696798594dabd0454d7c5f046c8cafa022d9c
jeniferirwin <jenifer.irwin@gmail.com>
2021-01-29 10:05:55 -0500

    Release 1.0

---
+++ Assets/Scripts/Core/EndingScreenStarter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.Technitaur.GreenBean.Core
+{
+    public class EndingScreenStarter : MonoBehaviour
+    {
+        public void Awake()
+        {
+            SceneManager.LoadScene("Ending");
+        }
+    }
+}
